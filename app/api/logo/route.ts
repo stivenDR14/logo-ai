e185da65350854d204d5f2080c56dba28bc7d92f
enzo <eesteves@hotmail.fr>
2024-09-20 10:45:33 -0400

    test

--- app/api/logo/route.ts
+++ app/api/logo/route.ts
@@ -1,14 +1,15 @@
 import prisma from "@/_utils/prisma";
 
 export async function GET() {
-  const images = await prisma.logo.findMany({
-    select: {
-      id: true,
-    },
-    take: 24,
-    orderBy: {
-      id: "desc",
-    },
-  });
-  return Response.json(images.map((image) => image.id));
+  // const images = await prisma.logo.findMany({
+  //   select: {
+  //     id: true,
+  //   },
+  //   take: 24,
+  //   orderBy: {
+  //     id: "desc",
+  //   },
+  // });
+  // return Response.json(images.map((image) => image.id));
+  return Response.json([]);
 }
